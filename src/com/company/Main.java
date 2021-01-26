63017273c3ba370633cfd3c6e71600e9dce4675a
Hyeeun Lee <21545@student.dorset-college.ie>
2021-01-26 15:57:53 +0000

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,41 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        final String DB_URL = "jdbc:mysql://localhost:3306/titanicmanifest";
+        final String DB_USER = "root";
+        final String DB_PASSWORD = "ah447Sladl!";
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/titanicmanifest", "root", "ah447Sladl!");
+            Connection con = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from passengers");
+            String sqlQuery = "select * from titanic";
+            ResultSet rs = stmt.executeQuery(sqlQuery);
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            for (int i = 1; i <= numberOfColumns; i++){
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            }
+            System.out.println();
 
             while (rs.next())
-                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                System.out.println(rs.getInt(1) + "\t " + rs.getInt(2) + "\t " + rs.getInt(3)  + "\t " + rs.getString(4));
             con.close();
 
-        } catch (Exception e) {
+        }
+        catch (SQLException e){
             System.out.println(e.toString());
+        }
+        catch (Exception e) {
+
 
         } finally {
 
